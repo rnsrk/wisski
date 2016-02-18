6ba9ecd1cc7273d2774df96a92b5a5f0af480155
domerz@wisski <dorian.merz@fau.de>
2016-02-18 14:54:26 +0100

    WisskiQueryBase

---
+++ wisski_core/src/Query/WisskiQueryBase.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\wisski_core\Query;
+
+class WisskiQueryBase implements WisskiQueryInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function hasEntity($entity_id) {
+    return ($entity_id == 42);
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  protected function getExternalID($entity_id) {
+  
+    return '42';
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function loadFieldValues($entity_id,$field_name,$language = LanguageInterface::LANGCODE_DEFAULT,$field_property = NULL) {
+
+    if (isset($field_property)) {
+      return 'Hello';
+    }  
+    return array('value'=>'Ciao');
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function saveFieldValues($entity_id,$field_name,array $values) {
+    
+    return SAVED_NEW;
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function hasFieldData($entity_id,$field_name,$field_property = NULL) {
+    
+    return ($entity_id == 42);
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function getBundlesForEntity($entity_id) {
+  
+    //this is a hardcoded example bundle, should exist in your Drupal
+    return 'e21_person';
+  }
+}
