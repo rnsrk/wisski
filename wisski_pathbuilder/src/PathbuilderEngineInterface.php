8bfd82850e07c7fd4420cba838c10ae5d6a0dbd1
Martin Scholz <martin.scholz@fau.de>
2016-03-18 15:04:19 +0100

    Added basic functionality for pb connex to adapter

---
+++ wisski_pathbuilder/src/PathbuilderEngineInterface.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\wisski_pathbuilder\WisskiPathInterface
+ */
+
+namespace Drupal\wisski_pathbuilder;
+
+use Drupal\wisski_salz\EngineInterface;
+
+/**
+ * Provides an interface defining a pathbuilder path entity type.
+ */
+interface PathbuilderEngineInterface extends EngineInterface {
+  
+  /**
+   * Returns a list of possible steps between history of steps and future of
+   * steps.
+   *
+   * @param history An array of the previous steps or an empty array if this is
+   *  the beginning of the path.
+   * @param future An array of the following steps or an empty array if this is 
+   *  (currently!) the last step.
+   *
+   * @return array
+   *  A list of steps
+   *
+   */
+  public function getPathAlternatives($history = [], $future = []);
+  
+  
+  /**
+   * Returns human readable information for a step.
+   *
+   * @param step the step
+   *
+   * @return an array with following keys
+   *  label : a human readable label, translatable, one line
+   *  description : a description of the step, translatable, multiline
+   */
+  public function getStepInfo($step, $history = [], $future = []);
+
+}
