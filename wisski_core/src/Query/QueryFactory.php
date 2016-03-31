3111143c0da907c310678dd05abff0bfd01f0f90
WissKI <m.fichtner@wiss-ki.eu>
2016-03-31 16:06:55 +0200

    Added iterating through all adapters

--- wisski_core/src/Query/QueryFactory.php
+++ wisski_core/src/Query/QueryFactory.php
@@ -26,15 +26,17 @@ class QueryFactory implements QueryFactoryInterface {
    * {@inheritdoc}
    */
   public function get(EntityTypeInterface $entity_type, $conjunction) {
-    
+
+
 //    dpm(func_get_args(),__METHOD__);
     $adapters = entity_load_multiple('wisski_salz_adapter');
-    //
-    // WATCH OUT - nasty assumption of yaml adapter being main store
-    // @TODO change that
-    //
-    $adapter = $adapters['wisski_yaml_adapter'];
-    $query = $adapter->getQueryObject($entity_type,$conjunction,$this->namespaces);
+
+    // iterate through all adapters and go for it.
+    // Nasty assumption - this might break due to stupidity of the programmers.
+    // Enable super magic main query as master thing whatever something...
+    foreach($adapters as $adapter) {    
+      $query = $adapter->getQueryObject($entity_type,$conjunction,$this->namespaces);
+    }
 //    dpm($query);
     return $query;
   }
