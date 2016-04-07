4591ad8ac9b2280744ad347e73db7880d95baa32
domerz@wisski <dorian.merz@fau.de>
2016-04-07 12:59:07 +0200

    revert again

--- wisski_salz/src/Query/WisskiQueryBase.php
+++ wisski_salz/src/Query/WisskiQueryBase.php
@@ -8,19 +8,19 @@ use Drupal\Core\Entity\Query\QueryBase;
 use Drupal\Core\Entity\Query\QueryInterface;
 use Drupal\Core\Entity\Query\QueryAggregateInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
-use Drupal\wisski_salz\AdapterInterface;
+use Drupal\wisski_salz\EngineInterface;
 
 abstract class WisskiQueryBase extends QueryBase implements QueryInterface, QueryAggregateInterface {
 
-  protected $parent_adapter;
+  protected $parent_engine;
 
-  public function __construct(EntityTypeInterface $entity_type,$condition,array $namespaces,AdapterInterface $parent_adapter) {
+  public function __construct(EntityTypeInterface $entity_type,$condition,array $namespaces,EngineInterface $parent_engine) {
     $namespaces = array_merge($namespaces,QueryBase::getNamespaces($this));
     parent::__construct($entity_type,$condition,$namespaces);
     $this->parent_adapter = $parent_adapter;
   }
   
-  public function getAdapter() {
-    return $this->parent_adapter;
+  public function getEngine() {
+    return $this->parent_engine;
   }
 }
