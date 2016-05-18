de696878887f90fd0be63d9b52897b90bb5cedf5
WissKI <m.fichtner@wiss-ki.eu>
2016-05-18 09:01:31 +0200

    updates

--- wisski_core/src/Entity/WisskiBundle.php
+++ wisski_core/src/Entity/WisskiBundle.php
@@ -15,10 +15,11 @@ use Drupal\wisski_core\WisskiBundleInterface;
  *     "form" = {
  *       "add" = "Drupal\wisski_core\Form\WisskiBundleForm",
  *       "edit" = "Drupal\wisski_core\Form\WisskiBundleForm",
- *			 "delete" = "Drupal\wisski_core\Form\WisskiBundleDeleteForm",
+ *	 "delete" = "Drupal\wisski_core\Form\WisskiBundleDeleteForm",
+ *       "title" = "Drupal\wisski_core\Form\WisskiTitlePatternForm",
  *     },
  *     "list_builder" = "Drupal\wisski_core\Controller\WisskiBundleListBuilder",
- *		 "access" = "Drupal\wisski_core\Controller\WisskiBundleAccessHandler",
+ *     "access" = "Drupal\wisski_core\Controller\WisskiBundleAccessHandler",
  *   },
  *   admin_permission = "administer wisski_core",
  *   config_prefix = "bundle",
@@ -26,24 +27,23 @@ use Drupal\wisski_core\WisskiBundleInterface;
  *   entity_keys = {
  *     "id" = "id",
  *     "label" = "label",
- *		 "description" = "description",
+ *     "description" = "description",
  *   },
  *   links = {
  *     "edit-form" = "/admin/structure/wisski_core/{wisski_bundle}/edit",
  *     "delete-form" = "/admin/structure/wisski_core/{wisski_bundle}/delete",
  *     "entity-list" = "/admin/structure/wisski_core/{wisski_bundle}/list",
- *		 "list" = "/admin/structure/wisski_core",
+ *     "list" = "/admin/structure/wisski_core",
  *   }
  * )
  */
 class WisskiBundle extends ConfigEntityBundleBase implements WisskiBundleInterface {
   
-  private $title_pattern_string = '';
-  private $title_pattern_matches = array();
+  private $title_pattern = array();
   
   public function getTitlePattern() {
     
-    return array($this->title_pattern_string,$this->title_pattern_matches);
+    return $this->title_pattern;
   }
   
   public function setTitlePattern($title_pattern) {
@@ -55,25 +55,6 @@ class WisskiBundle extends ConfigEntityBundleBase implements WisskiBundleInterfa
   
   protected function isValidTitlePattern($title_pattern) {
   
-    if (empty($title_pattern)) {
-      $this->title_pattern_string = '';
-      $this->title_pattern_matches = array();
-      return TRUE;
-    }
-    $regex = '\%(\w+)\%';
-    $matches = array();
-    if (preg_match_all('/'.$regex.'/',$title_pattern,$matches)) {
-      $available_names = array_keys(\Drupal::entityManager()->getFieldStorageDefinitions('wisski_individual',$this->id));
-      $diff = array_diff($matches[1],$available_names);
-      dpm($matches,'Matches');
-      if (empty($diff)) {
-        $this->title_pattern_matches = $matches[1];
-        return TRUE;
-      } else {
-        drupal_set_message(t('Inavlid field names used: %names',array('%names' => implode(', ',$diff))),'error');
-        return FALSE;
-      }
-    }
     return TRUE;
   }
 }
