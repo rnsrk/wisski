ff6bdf85a8529e707d2134ca2ed9fd29f1eee3b7
WissKI <m.fichtner@wiss-ki.eu>
2016-05-30 12:00:53 +0200

    Edited WisskiPathForm

--- wisski_iip_image/src/Plugin/Field/FieldFormatter/WisskiIIPImageFormatter.php
+++ wisski_iip_image/src/Plugin/Field/FieldFormatter/WisskiIIPImageFormatter.php
@@ -6,9 +6,18 @@
    
   namespace Drupal\wisski_iip_image\Plugin\Field\FieldFormatter;
    
+  use Drupal\Core\Entity\EntityStorageInterface;
   use Drupal\Core\Field\FieldItemListInterface;
-  use Drupal\Core\Field\FormatterBase;
-  use Drupal;
+  use Drupal\Core\Field\FieldDefinitionInterface;
+  use Drupal\Core\Link;
+  use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+  use Drupal\Core\Session\AccountInterface;
+  use Drupal\Core\Url;
+  use Drupal\image\Entity\ImageStyle;
+  use Symfony\Component\DependencyInjection\ContainerInterface;
+  use Drupal\Core\Form\FormStateInterface;
+  use Drupal\Core\Cache\Cache;
+  use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
   
   /**
    * Plugin implementation of the 'wisski_iip_image' formatter.
@@ -22,20 +31,80 @@
    *   }
    * )
    */
-  class WisskiIIPImageFormatter extends FormatterBase {
+  class WisskiIIPImageFormatter extends ImageFormatterBase {
     /**
      * {@inheritdoc}
      */
-    public function viewElements(Drupal\Core\Field\FieldItemListInterface $items, $langcode) {
+    public function viewElements(FieldItemListInterface $items, $langcode) {
+      
       $elements = array();
-      dpm($items);
-#      $countries = \Drupal::service('country_manager')->getList();
-#      foreach ($items as $delta => $item) {
-#        if (isset($countries[$item->value])) {
-#          $elements[$delta] = array('#markup' => $countries[$item->value]);
-#        }
-#      }
+      $files = $this->getEntitiesToView($items, $langcode);
+#     drupal_set_message(serialize($files));
+
+    // Early opt-out if the field is empty.
+    if (empty($files)) {
       return $elements;
+    }
+
+    $url = NULL;
+    $image_link_setting = $this->getSetting('image_link');
+    // Check if the formatter involves a link.
+    if ($image_link_setting == 'content') {
+      $entity = $items->getEntity();
+      if (!$entity->isNew()) {
+        $url = $entity->urlInfo();
+      }
+    }
+    elseif ($image_link_setting == 'file') {
+      $link_file = TRUE;
+    }
+
+    $image_style_setting = $this->getSetting('image_style');
+
+    // Collect cache tags to be added for each item in the field.
+    $cache_tags = array();
+    if (!empty($image_style_setting)) {
+      $image_style = $this->imageStyleStorage->load($image_style_setting);
+      $cache_tags = $image_style->getCacheTags();
+    }
+
+    foreach ($files as $delta => $file) {
+      $cache_contexts = array();
+      if (isset($link_file)) {
+        $image_uri = $file->getFileUri();
+        // @todo Wrap in file_url_transform_relative(). This is currently
+        // impossible. As a work-around, we currently add the 'url.site' cache
+        // context to ensure different file URLs are generated for different
+        // sites in a multisite setup, including HTTP and HTTPS versions of the
+        // same site. Fix in https://www.drupal.org/node/2646744.
+        $url = Url::fromUri(file_create_url($image_uri));
+        $cache_contexts[] = 'url.site';
+      }
+      $cache_tags = Cache::mergeTags($cache_tags, $file->getCacheTags());
+
+      // Extract field item attributes for the theme function, and unset them
+      // from the $item so that the field template does not re-render them.
+      $item = $file->_referringItem;
+      $item_attributes = $item->_attributes;
+      unset($item->_attributes);
+
+      drupal_set_message("url: " . serialize($item));
+
+      $elements[$delta] = array(
+        '#theme' => 'image_formatter',
+        '#item' => $item,
+        '#item_attributes' => $item_attributes,
+        '#image_style' => $image_style_setting,
+        '#url' => $url,
+        '#cache' => array(
+          'tags' => $cache_tags,
+          'contexts' => $cache_contexts,
+        ),
+      );
+    }
+
+    return $elements;
+
     }
     
 
