755b5da0f43c1359ffc97c68ac28ed906cc29202
WissKI <m.fichtner@wiss-ki.eu>
2016-06-06 13:48:48 +0200

    Added everything

--- wisski_iip_image/js/iip_integration.js
+++ wisski_iip_image/js/iip_integration.js
@@ -1,17 +1,18 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
+  var server = "/fcgi-bin/iipsrv.fcgi";
+  var images = ["/srv/www/htdocs/dev/sites/default/files/de514d90b6b6862363818e117ebf7e59.tif"];
+  var credit = '&copy; <a href="http://www.gnm.de/">Germanisches Nationalmuseum</a>';
+  var iipmooviewer = new IIPMooViewer( "cboxContent", {
+    image: images,
+    server: server,
+    credit: credit,
+//    prefix: \'' . $base_path . drupal_get_path('module', 'wisski_iip') . '/iipmooviewer/images/\',
+//    ' . $scale . '
+    showNavWindow: true,
+    showNavButtons: true,
+    winResize: true,
+    protocol: 'iip',
+  });
+  
   alert("Hallo welt!");
-/*        var server = "/fcgi-bin/iipsrv.fcgi";
-                var images = ["' . $imagepath . '"];
-                        var credit = \'&copy; <a href="http://www.gnm.de/">Germanisches Nationalmuseum</a>\';
-                                var iipmooviewer = new IIPMooViewer( "viewer", {
-                                          image: images,
-                                                    server: server,
-                                                              credit: credit,
-                                                                        prefix: \'' . $base_path . drupal_get_path('module', 'wisski_iip') . '/iipmooviewer/images/\',
-                                                                                  ' . $scale . '
-                                                                                            showNavWindow: true,
-                                                                                                      showNavButtons: true,
-                                                                                                                winResize: true,
-                                                                                                                          protocol: \'iip\',
-                                                                                                                                  });*/
-                                                                                                                                        });
+});
