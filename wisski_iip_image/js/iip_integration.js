135fe4c853df9d3965cf4074b81b3c8ab825e54e
WissKI <m.fichtner@wiss-ki.eu>
2016-06-06 10:56:42 +0200

    added iip js

---
+++ wisski_iip_image/js/iip_integration.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  alert("Hallo welt!");
+/*        var server = "/fcgi-bin/iipsrv.fcgi";
+                var images = ["' . $imagepath . '"];
+                        var credit = \'&copy; <a href="http://www.gnm.de/">Germanisches Nationalmuseum</a>\';
+                                var iipmooviewer = new IIPMooViewer( "viewer", {
+                                          image: images,
+                                                    server: server,
+                                                              credit: credit,
+                                                                        prefix: \'' . $base_path . drupal_get_path('module', 'wisski_iip') . '/iipmooviewer/images/\',
+                                                                                  ' . $scale . '
+                                                                                            showNavWindow: true,
+                                                                                                      showNavButtons: true,
+                                                                                                                winResize: true,
+                                                                                                                          protocol: \'iip\',
+                                                                                                                                  });*/
+                                                                                                                                        });
