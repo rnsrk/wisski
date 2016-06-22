a5e032ec6a8756d1a4e01e78a27f665d26b5ec24
WissKI <m.fichtner@wiss-ki.eu>
2016-06-22 18:29:16 +0200

    Corrected regex

--- wisski_adapter_sparql11_pb/src/Plugin/wisski_salz/Engine/Sparql11EngineWithPB.php
+++ wisski_adapter_sparql11_pb/src/Plugin/wisski_salz/Engine/Sparql11EngineWithPB.php
@@ -1464,24 +1464,24 @@ class Sparql11EngineWithPB extends Sparql11Engine implements PathbuilderEngineIn
             $op = '!=';
           if($op == 'STARTS_WITH') {
             $regex = true;
-            $primitiveValue = '*' . $primitiveValue;
+            $primitiveValue = '^' . $primitiveValue;
           }
           
           if($op == 'ENDS_WITH') {
             $regex = true;
-            $primitiveValue = '' . $primitiveValue . '*';
+            $primitiveValue = '' . $primitiveValue . '$';
           }
           
           if($op == 'CONTAINS') {
             $regex = true;
-            $primitiveValue = '*' . $primitiveValue . '*';
+            $primitiveValue = '' . $primitiveValue . '", "i';
           }
           
         
           if($regex)
             $query .= ' ?out . FILTER ( regex ( ?out, "' . $primitiveValue . '" ) ) . ';
           else
-            $query .= ' ?out . FILTER ( ?out ' . $op . ' ' . $primitiveValue . ' ) . ';
+            $query .= ' ?out . FILTER ( ?out ' . $op . ' "' . $primitiveValue . '" ) . ';
         }
       } else
         $query .= " ?out . ";
