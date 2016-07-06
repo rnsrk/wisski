c6754250b4709fca584cbdce5a8ad3167b0f9234
Martin Scholz <martin.scholz@fau.de>
2016-07-06 15:50:50 +0200

    rename var

--- wisski_adapter_sparql11_pb/src/Plugin/wisski_salz/Engine/Sparql11EngineWithPB.php
+++ wisski_adapter_sparql11_pb/src/Plugin/wisski_salz/Engine/Sparql11EngineWithPB.php
@@ -1338,7 +1338,7 @@ class Sparql11EngineWithPB extends Sparql11Engine implements PathbuilderEngineIn
     
     if(!empty($entity_id)) {
       // rename to uri
-      $eid = $this->getUriForDrupalId($entiy_id);    
+      $eid = $this->getUriForDrupalId($entity_id);    
 #      $eid = str_replace("\\", "/", $entity_id);
       $url = parse_url($eid);
       
