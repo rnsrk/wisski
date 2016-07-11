7938d40ac14a9aa3cfceb6b6fdeab6b26d60a4f0
WissKI <m.fichtner@wiss-ki.eu>
2016-07-11 10:32:37 +0200

    Added Create

--- wisski_core/src/Controller/WisskiEntityListController.php
+++ wisski_core/src/Controller/WisskiEntityListController.php
@@ -10,8 +10,12 @@ use Drupal\Core\Entity\Controller\EntityListController;
 class WisskiEntityListController extends EntityListController {
 
   public function listing($wisski_bundle=NULL,$wisski_individual=NULL) {
-    
-    if (is_null($wisski_bundle)) return $this->entityManager()->getListBuilder('wisski_bundle')->render(WisskiBundleListBuilder::NAVIGATE);
+#    dpm($this->getDestinationArray());
+    if (is_null($wisski_bundle)) {
+      if(strpos($this->getDestinationArray()['destination'], 'create') !== FALSE)
+        return $this->entityManager()->getListBuilder('wisski_bundle')->render(WisskiBundleListBuilder::CREATE);
+      return $this->entityManager()->getListBuilder('wisski_bundle')->render(WisskiBundleListBuilder::NAVIGATE);
+    }
     return $this->entityManager()->getListBuilder('wisski_individual')->render($wisski_bundle,$wisski_individual);
   }
 }
