5f3789eb70abc1dd9e08a12aa6111c86fcf08009
Knurg <m.fichtner@wiss-ki.eu>
2017-03-07 10:48:09 +0100

    Added better Count support.

--- wisski_adapter_sparql11_pb/src/Query/Query.php
+++ wisski_adapter_sparql11_pb/src/Query/Query.php
@@ -361,7 +361,7 @@ class Query extends WisskiQueryBase {
   protected function buildAndExecSparql($query_parts, $entity_ids, $count = FALSE, $limit = 0, $offset = 0) {
     
     if ($count) {
-      $select = 'SELECT (COUNT(?x0) as ?cnt) WHERE { ';
+      $select = 'SELECT (COUNT(DISTINCT ?x0) as ?cnt) WHERE { ';
     }
     else {
       $select = 'SELECT DISTINCT ?x0 WHERE { ';
@@ -382,6 +382,7 @@ class Query extends WisskiQueryBase {
     }
 
     $result = $engine = $this->getEngine()->directQuery($select);
+#    drupal_set_message(serialize($select));
     $adapter_id = $this->getEngine()->adapterId();
     if (WISSKI_DEVEL) \Drupal::logger("query adapter $adapter_id")->debug('(sub)query {query} yielded {result}', array('query' => $select, 'result' => $result));
     if ($result->numRows() == 0) {
@@ -405,7 +406,7 @@ class Query extends WisskiQueryBase {
         }
       }
     }
-
+#    drupal_set_message(serialize($return));
     return $return;
 
   }
