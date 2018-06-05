2ef77558728ed282b5ba80fe2780d3d8052b83a2
Knurg <m.fichtner@wiss-ki.eu>
2018-06-05 18:13:12 +0200

    Added read more capability.

--- wisski_adapter_sparql11_pb/src/Plugin/Field/FieldFormatter/WisskiLinkFormatter.php
+++ wisski_adapter_sparql11_pb/src/Plugin/Field/FieldFormatter/WisskiLinkFormatter.php
@@ -15,6 +15,7 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Url;
 use Drupal\wisski_salz\AdapterHelper;
+use Drupal\Component\Utility\Html;
    
 /**
  * Plugin implementation of the 'wisski_link_formatter' formatter.
@@ -65,8 +66,22 @@ class WisskiLinkFormatter extends FormatterBase implements ContainerFactoryPlugi
    */
   public static function defaultSettings() {
     return array(
+      // Add default settings with text, so we can wrap them in t().
+      // in milliseconds.
+      'speed' => 100,
+      'collapsedHeight' => 75,
+      // In pixel.
+      'heightMargin' => 16,
+      'moreLink' => '<a href="#">' . t('Read more') . '</a>',
+      'lessLink' => '<a href="#">' . t('Close') . '</a>',
+      'embedCSS' => 1,
+      'sectionCSS' => 'display: block; width: 100%;',
+      'startOpen' => 0,
+      'expandedClass' => 'readmore-js-expanded',
+      'collapsedClass' => 'readmore-js-collapsed',
       'imagecache_external_style' => '',
       'imagecache_external_link' => '',
+      'use_readmore' => 1,
     ) + parent::defaultSettings();
   }
 
@@ -97,6 +112,86 @@ class WisskiLinkFormatter extends FormatterBase implements ContainerFactoryPlugi
       '#empty_option' => t('Nothing'),
       '#options' => $link_types,
     );
+    
+    $elements['use_readmore'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Use readmore'),
+      '#description' => $this->t('Do you want to use readmore?'),
+      '#default_value' => $this->getSetting('use_readmore'),
+    ];
+
+    $elements['speed'] = [
+      '#type' => 'number',
+      '#min' => 1,
+      '#title' => $this->t('Speed'),
+      '#description' => $this->t('Speed for show / hide read more.'),
+      '#default_value' => $this->getSetting('speed'),
+    ];
+
+    $elements['collapsedHeight'] = [
+      '#type' => 'number',
+      '#min' => 1,
+      '#title' => $this->t('Collapsed Height'),
+      '#description' => $this->t('Height after which readmore will be added.'),
+      '#default_value' => $this->getSetting('collapsedHeight'),
+    ];
+
+    $elements['heightMargin'] = [
+      '#type' => 'number',
+      '#min' => 1,
+      '#title' => $this->t('Height margin'),
+      '#description' => $this->t('Avoids collapsing blocks that are only slightly larger than maxHeight.'),
+      '#default_value' => $this->getSetting('heightMargin'),
+    ];
+
+    $elements['moreLink'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('More link'),
+      '#description' => $this->t('Link for more.'),
+      '#default_value' => $this->getSetting('moreLink'),
+    ];
+
+    $elements['lessLink'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Less link'),
+      '#description' => $this->t('Link for less.'),
+      '#default_value' => $this->getSetting('lessLink'),
+    ];
+
+    $elements['embedCSS'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Embed CSS'),
+      '#description' => $this->t('Insert required CSS dynamically, set this to false if you include the necessary CSS in a stylesheet.'),
+      '#default_value' => $this->getSetting('embedCSS'),
+    ];
+
+    $elements['sectionCSS'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Section styling'),
+      '#description' => $this->t('Sets the styling of the blocks, ignored if embedCSS is false).'),
+      '#default_value' => $this->getSetting('sectionCSS'),
+    ];
+
+    $elements['startOpen'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Start open'),
+      '#description' => $this->t('Do not immediately truncate, start in the fully opened position.'),
+      '#default_value' => $this->getSetting('startOpen'),
+    ];
+
+    $elements['expandedClass'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Expanded class'),
+      '#description' => $this->t('Class added to expanded blocks.'),
+      '#default_value' => $this->getSetting('expandedClass'),
+    ];
+
+    $elements['collapsedClass'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Collapsed class'),
+      '#description' => $this->t('Class added to collapsed blocks.'),
+      '#default_value' => $this->getSetting('collapsedClass'),
+    ];
 
     return $elements;
   }
@@ -133,6 +228,17 @@ class WisskiLinkFormatter extends FormatterBase implements ContainerFactoryPlugi
     if (isset($link_types[$settings['imagecache_external_link']])) {
       $summary[] = $link_types[$settings['imagecache_external_link']];
     }
+    
+    $summary[] = $this->t('Speed: @value', ['@value' => $this->getSetting('speed')]);
+    $summary[] = $this->t('Collapsed Height: @value', ['@value' => $this->getSetting('collapsedHeight')]);
+    $summary[] = $this->t('Height margin: @value', ['@value' => $this->getSetting('heightMargin')]);
+    $summary[] = $this->t('More link: @value', ['@value' => $this->getSetting('moreLink')]);
+    $summary[] = $this->t('Less link: @value', ['@value' => $this->getSetting('lessLink')]);
+    $summary[] = $this->t('Embed CSS: @value', ['@value' => $this->getSetting('embedCSS')]);
+    $summary[] = $this->t('Section styling: @value', ['@value' => $this->getSetting('sectionCSS')]);
+    $summary[] = $this->t('Start open: @value', ['@value' => $this->getSetting('startOpen')]);
+    $summary[] = $this->t('Expanded class: @value', ['@value' => $this->getSetting('expandedClass')]);
+    $summary[] = $this->t('Collapsed class: @value', ['@value' => $this->getSetting('collapsedClass')]);
 
     return $summary;
   }
@@ -150,7 +256,7 @@ class WisskiLinkFormatter extends FormatterBase implements ContainerFactoryPlugi
     $elements = [];
 #    drupal_set_message(serialize($items));
 
-#    drupal_set_message("yay!");
+#    drupal_set_message("yay!" . microtime());
     
     foreach($items as $delta => $item) {
       $values = $item->toArray();
@@ -247,6 +353,39 @@ class WisskiLinkFormatter extends FormatterBase implements ContainerFactoryPlugi
 
     }
   */
+
+  
+    if($settings['use_readmore']) {
+  
+      $integer_fields = [
+        'speed',
+        'collapsedHeight',
+        'heightMargin',
+        'embedCSS',
+        'startOpen',
+      ];
+      foreach ($integer_fields as $key) {
+        $settings[$key] = (int) $settings[$key];
+      }
+      $field_name = $items->getFieldDefinition()->getName();
+    #foreach ($items as $delta => $item) {
+#      dpm($elements, "elements");
+
+      if(!empty($elements)) {
+        $unique_id = Html::getUniqueId('field-readmore-' . $field_name);
+#        $elements['#prefix'] = '<div class="field-readmore ' . $unique_id . '">';
+#        $elements['#suffix'] = '</div>';
+
+        if(!empty($elements['#attributes']) && !empty($elements['#attributes']['class'])) {
+          $elements['#attributes']['class'] = array_merge($elements['#attributes']['class'], array("field-readmore " . $unique_id));
+        } else {
+          $elements['#attributes']['class'] = array("field-readmore " . $unique_id);
+        }
+        $elements['#attached']['library'][] = 'wisski_adapter_sparql11_pb/readmore';
+        $elements['#attached']['drupalSettings']['readmoreSettings'][$unique_id] = $settings;
+      }
+    }
+  
     return $elements;
   
   }
