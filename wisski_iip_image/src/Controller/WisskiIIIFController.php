965903a8857ac8fd57e1fd4ade7755454f7e707c
Knurg <m.fichtner@wiss-ki.eu>
2018-10-05 22:23:34 +0200

    Further building a basis

--- wisski_iip_image/src/Controller/WisskiIIIFController.php
+++ wisski_iip_image/src/Controller/WisskiIIIFController.php
@@ -3,7 +3,7 @@
 namespace Drupal\wisski_iip_image\Controller;
 
 use Drupal\Core\Entity\ContentEntityStorageInterface;
-
+use Drupal\Core\Cache\CacheableJsonResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
@@ -11,8 +11,20 @@ class WisskiIIIFController {
 
   public function manifest(\Drupal\wisski_core\Entity\WisskiEntity $wisski_individual = NULL) {
 
-    echo "hallo welt!";
+    $data = [];
 
+    $data[] = "Hallo Welt!";
+
+    $data['#cache'] = [
+      'max-age' => 1, 
+      'contexts' => [
+         'url',
+      ],
+    ];
+
+    $response = new CacheableJsonResponse($data);
+
+    return $response;
   }
   
 }
