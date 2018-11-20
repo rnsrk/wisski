3eb8033b68e4091e4af632683560b960ab82abd3
Knurg <m.fichtner@wiss-ki.eu>
2018-11-20 17:36:48 +0100

    Added a bugfix for slow loading - dont know why?!

--- wisski_core/src/Plugin/views/filter/FieldString.php
+++ wisski_core/src/Plugin/views/filter/FieldString.php
@@ -21,39 +21,47 @@ class FieldString extends ViewsString {
   function operators() {
     $operators = parent::operators();
     $operators_new = array(
+    /*
       '=' => array(
         'title' => t('Is equal to'),
         'short' => t('='),
-        'method' => 'opSimple',
+        'method' => 'opEqual',
         'values' => 1,
       ),
       '!=' => array(
         'title' => t('Is not equal to'),
         'short' => t('!='),
-        'method' => 'opSimple',
+        'method' => 'opEqual',
         'values' => 1,
       ),
       'CONTAINS' => array(
         'title' => t('Contains'),
         'short' => t('contains'),
-        'method' => 'opSimple',
+        'method' => 'opContains',
         'values' => 1,
       ),
       'STARTS_WITH' => array(
         'title' => t('Starts with'),
         'short' => t('begins'),
-        'method' => 'opSimple',
+        'method' => 'opStartsWith',
         'values' => 1,
       ),
       'ENDS_WITH' => array(
         'title' => t('Ends with'),
         'short' => t('ends'),
-        'method' => 'opSimple',
+        'method' => 'opEndsWith',
         'values' => 1,
       ),
+    */
       'EMPTY' => array(
         'title' => t('Is empty'),
         'short' => t('empty'),
+        'method' => 'opSimple',
+        'values' => 1,
+      ),
+      'NOT_EMPTY' => array(
+        'title' => t('Is not empty'),
+        'short' => t('not_empty'),
         'method' => 'opSimple',
         'values' => 1,
       ),
@@ -64,6 +72,8 @@ class FieldString extends ViewsString {
         'values' => 1,
       ),
     );
+
+#    dpm($operators, "old");
     
     $operators = array_merge($operators, $operators_new);
 
