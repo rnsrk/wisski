d38a5824fe4e1e4d1adc7716e006c4e64e2cca0c
Tom Wiesing <tkw01536@gmail.com>
2020-10-09 15:01:37 +0200

    Add 'query' operation to AdapterListBuilder
    This commit adds a new 'query' operation to the sparql adapter
    AdapterListBuilder. It links to the querying interface of each endpoint.

--- wisski_salz/src/AdapterListBuilder.php
+++ wisski_salz/src/AdapterListBuilder.php
@@ -9,11 +9,31 @@ namespace Drupal\wisski_salz;
 
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Url;
 
 /**
  * Provides a listing of WissKI Salz Adapter entities.
  */
 class AdapterListBuilder extends ConfigEntityListBuilder {
+
+/**
+ * {@inheritdoc}
+ */
+  public function getOperations(EntityInterface $entity) {
+    $operations = $this->getDefaultOperations($entity);
+
+    // add an operation for querying the endpoint
+    $operations['query'] = array(
+      'title' => $this->t('Query'),
+      'weight' => 50,
+      'url' => Url::fromRoute('wisski_adapter_sparql11_pb.wisski_endpoint', array('endpoint_id' => $entity->id())),
+    );
+
+    // keep things sorted, so that it shows up in the middle
+    uasort($operations, '\\Drupal\\Component\\Utility\\SortArray::sortByWeightElement');
+    return $operations;
+}
+
   /**
    * {@inheritdoc}
    */
