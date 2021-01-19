b69045bcdf93901ade9abc525696a955af0697d5
Selphie <My.Fi@gmx.de>
2021-01-19 19:05:31 +0100

    entity references have no language and thus in case of changes, items that should be deleted were not deleted properly from the system; see code comment for more information

--- wisski_adapter_sparql11_pb/src/Plugin/wisski_salz/Engine/Sparql11EngineWithPB.php
+++ wisski_adapter_sparql11_pb/src/Plugin/wisski_salz/Engine/Sparql11EngineWithPB.php
@@ -1184,26 +1184,20 @@ class Sparql11EngineWithPB extends Sparql11Engine implements PathbuilderEngineIn
    * The elemental data aggregation function
    * fetches the data for display purpose
    */
-  public function pathToReturnValue($path, $pb, $eid = NULL, $position = 0, $main_property = NULL, $relative = TRUE, $language = LanguageInterface::LANGCODE_DEFAULT) {
+  public function pathToReturnValue($path, $pb, $eid = NULL, $position = 0, $main_property = NULL, $relative = TRUE, $language = LanguageInterface::LANGCODE_DEFAULT ) {
 #    drupal_set_message("I got: $eid " . serialize($path));
 #$tmpt1 = microtime(TRUE);            
 #  drupal_set_message("ptrv: " . microtime());
-
-    //MyF: Instead of the LANGCODE_DEFAULT (x-default), we use the displayed language
-    $language = \Drupal::service('language_manager')->getCurrentLanguage()->getId();
 
     $languages = array();
     
     if($language == "all") {
       $to_load = \Drupal::languageManager()->getLanguages();
       $languages = array_keys($to_load);
-#      dpm("these languages here are (if): " . serialize($languages));
       
 #      dpm($languages, "langs?");
     } else {
       $languages = array($language);
-#      dpm("and the language is: " . $language);
-#      dpm("these languages here are (else): " . serialize($languages));
     }
 
     if(empty($path)) {
@@ -1328,6 +1322,7 @@ class Sparql11EngineWithPB extends Sparql11Engine implements PathbuilderEngineIn
 //          dpm("I have no main_property");
 
           $my_language = $thing->out->getLang();
+          
           // if we have no language, take the default one.
 //          if(empty($my_language))
 //            $my_language = LanguageInterface::LANGCODE_DEFAULT;
@@ -1341,11 +1336,12 @@ class Sparql11EngineWithPB extends Sparql11Engine implements PathbuilderEngineIn
 #          dpm("Danger Zone!!", 
           $out[] = $thing->out->getValue();
         } else {
+          
           $my_language = $thing->out->getLang();
+          
           // if we have no language, take the default one.
           if(empty($my_language))
-          //MyF: if there is no language, we set the language to the current displayed language stored in $language (instead of setting it to LanguageInterface::LANGCODE_DEFAULT)
-            $my_language = $language;
+            $my_language = LanguageInterface::LANGCODE_DEFAULT;
           
           $outvalue = $thing->out->getValue();
 
@@ -1371,10 +1367,10 @@ class Sparql11EngineWithPB extends Sparql11Engine implements PathbuilderEngineIn
           }
         }
       } else {
-        //MyF: in this case, we take the language which is displayed (stored in $language)
-#       $my_language = LanguageInterface::LANGCODE_DEFAULT;
-        $my_language = $language;
-#        dpm("(Sparl11EngineWithPB.php, line 1380) I changed my lang from x-default to " . $my_language);
+      
+        // in this case we always take the language which is available...
+        
+        $my_language = LanguageInterface::LANGCODE_DEFAULT;
       
         if(empty($main_property)) {
           $out[] = $thing->{$name}->dumpValue("text");
@@ -1406,7 +1402,7 @@ class Sparql11EngineWithPB extends Sparql11Engine implements PathbuilderEngineIn
     // up to now out had the structure array( key => array( "value" => "some value that i've got from the Ts") )
     // in future we want that language dependent. This means we want:
     // array( "lang" = > array( key => array( "value" => "some value that i've got from the Ts") ) )
-  #  dpm("my out is: " . serialize($out));
+
     return $out;
     
   }
@@ -1429,7 +1425,6 @@ class Sparql11EngineWithPB extends Sparql11Engine implements PathbuilderEngineIn
     // paths in the pathbuilder.
 
 #    drupal_set_message("I am asked for " . serialize($entity_ids) . " and fields: " . serialize($field_ids));
-
 $tsa = ['start'=>microtime(true)];
     $pb_ids = array();
     $pb_man = \Drupal::service('wisski_pathbuilder.manager');
@@ -3064,7 +3059,7 @@ $tsa['ende'] = microtime(TRUE)-$tsa['start'];
             continue;
           
           // if not its a value...
-  #        drupal_set_message("I delete from " . $entity_id . " field " . $old_key . " value " . $val[$mainprop] . " key " . $key);
+        drupal_set_message("I delete from " . $entity_id . " field " . $old_key . " value " . $val[$mainprop] . " key " . $key);
           $this->deleteOldFieldValue($entity_id, $old_key, $val[$mainprop], $pathbuilder, $key, $mainprop, $language);
         }
       }
@@ -3126,7 +3121,7 @@ $tsa['ende'] = microtime(TRUE)-$tsa['start'];
               unset($write_values[$key]);
               continue;
             }
-#            drupal_set_message("old value is: " . serialize($old_value) . " new is: " . $new_item[$mainprop]);
+#           dpm("old value is: " . serialize($old_value) . " new is: " . $new_item[$mainprop]);
             // if the old value is somwhere in the new item
             if ($old_value == $new_item[$mainprop]) {
               // we unset the write value at this key because this doesn't have to be written
@@ -3150,9 +3145,11 @@ $tsa['ende'] = microtime(TRUE)-$tsa['start'];
               unset($delete_values[$old_key]);
               continue;
             }
-            
             // also if it does not have our language, we can simply skip it.
-            if ($old_item['wisski_language'] != $language) {
+            // MyF: here we also have to check whether a language is set; because in case of entity references there exist no wisski_language.
+            // In such cases, we may not do an unset because otherwise in case of changes (i.e. deleting a targeting entity reference), the old
+            // values would not be deleted properly in the code further down (Mostly fixed for the usage of the entity reference tree widget)
+            if (isset($old_item['wisski_language']) && $old_item['wisski_language'] != $language) {
               unset($delete_values[$old_key]);
               continue;
             }
@@ -3161,7 +3158,6 @@ $tsa['ende'] = microtime(TRUE)-$tsa['start'];
             $maincont = FALSE;
             
             foreach ($write_values as $key => $new_item) {
-            
               if (empty($new_item)) {
                 unset($write_values[$key]);
                 continue; // empty field item due to cardinality
@@ -3186,7 +3182,7 @@ $tsa['ende'] = microtime(TRUE)-$tsa['start'];
 #        dpm($delete_values, "we have to delete");
         if (!empty($delete_values)) {
           foreach ($delete_values as $key => $val) {            
-            #drupal_set_message("I1 delete from " . $entity_id . " field " . $old_key . " value " . $val[$mainprop] . " key " . $key);
+#            dpm("I1 delete from " . $entity_id . " field " . $old_key . " value " . $val[$mainprop] . " key " . $key);
             $this->deleteOldFieldValue($entity_id, $field_id, $val[$mainprop], $pathbuilder, $key, $mainprop, $language);
           }
         }
