06450d4284da20e1a16f181d7b2375d13184577c
Tom Wiesing <tkw01536@gmail.com>
2021-04-30 15:07:42 +0200

    more debug

--- wisski_salz/src/Query/WisskiQueryDelegator.php
+++ wisski_salz/src/Query/WisskiQueryDelegator.php
@@ -252,22 +252,21 @@ class WisskiQueryDelegator extends WisskiQueryBase {
     } 
     // create a new condition group and add conditions for all the children
     else if ($aast['type'] === ASTBuilder::TYPE_LOGICAL_AGGREGATE) {
-      try {
-        if ($aast['operator'] === "AND") {
-          $group = $condition->andConditionGroup();
-        } 
-        else if ($aast['operator'] === "OR") {
-          $group = $condition->orConditionGroup();
-        }
-        foreach ($aast['children'] as $child) {
-          $this->addConditionFromAst($group, $child);
-        }
-        $condition->condition($group);
-        return;
-      } catch(Exception $e) {
+      if (!method_exists($condition, 'andConditionGroup')) {
         dpm($condition, "condition");
         return;
       }
+      if ($aast['operator'] === "AND") {
+        $group = $condition->andConditionGroup();
+      } 
+      else if ($aast['operator'] === "OR") {
+        $group = $condition->orConditionGroup();
+      }
+      foreach ($aast['children'] as $child) {
+        $this->addConditionFromAst($group, $child);
+      }
+      $condition->condition($group);
+      return;
     }
     die("Implementation error!");
   }
