036c0996cfdea190b7276e89aacfec9e6830b3b1
philipp.eisenhuth <philipp.eisenhuth@uni-bayreuth.de>
2021-05-14 09:55:33 +0100

    wip

--- wisski_salz/src/Query/WisskiQueryDelegator.php
+++ wisski_salz/src/Query/WisskiQueryDelegator.php
@@ -364,7 +364,7 @@ class WisskiQueryDelegator extends WisskiQueryBase {
 
     foreach ($adapters as $adapter) {
       foreach ($pbsForBundle as $pb) {
-        $triplesForPath = $adapter->getEngine()->generateTriplesForPath($pb, $plan['ast']['annotations']['bundles'][0]);
+        $triplesForPath = $adapter->getEngine()->generateTriplesForPath($pb, $plan['ast']['annotations']['bundles'][0], $primitiveValue = "", $subject_in = NULL, $object_in = NULL, $disambposition = 0, $startingposition = 0, $write = FALSE, $op = '=', $mode = 'group', $relative = TRUE, $variable_prefixes = array(), $numbering = 0, $language = "und");
         dpm($triplesForPath, "triplesForpath");
       }
     }
