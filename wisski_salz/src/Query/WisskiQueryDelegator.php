ac7b058c21e91d605f30c9bd5be9b71392031af3
philipp.eisenhuth <philipp.eisenhuth@uni-bayreuth.de>
2021-05-14 11:24:00 +0100

    wip

--- wisski_salz/src/Query/WisskiQueryDelegator.php
+++ wisski_salz/src/Query/WisskiQueryDelegator.php
@@ -372,7 +372,6 @@ class WisskiQueryDelegator extends WisskiQueryBase {
     $pivotAdapter = current($adapters);
     dpm($pivotAdapter, "pivotAdapter");
     $triplesForPivotAdapter = "";
-      /*
     foreach ($adapters as $adapter) {
       foreach ($pbsForBundle as $pbArray) {
         //$pathId from bundleid
@@ -409,13 +408,14 @@ class WisskiQueryDelegator extends WisskiQueryBase {
                   GRAPH ?g_x0 { ?x0 a <http://erlangen-crm.org/200717/E21_Person> } .
                 }
               }
+            */
             
           }
           
         }
       }
     }
-    */
+    
     $sparql .= "}";
    
     
