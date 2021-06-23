515a314638c442e39bb4a71c8438124c2db20613
Knurg <m.fichtner@wiss-ki.eu>
2021-06-23 20:34:27 +0000

    Fixed issue for drupal 9 on cache helper

--- wisski_core/src/WisskiCacheHelper.php
+++ wisski_core/src/WisskiCacheHelper.php
@@ -235,7 +235,8 @@ class WisskiCacheHelper {
   static function flushAllPreviewImageUris() {
     
     //db_truncate('wisski_preview_images')->execute();
-    $options['target'] = 'default';
+    #$options['target'] = 'default';
+    $options = array();      
     // TODO: Drupal Rector Notice: Please delete the following comment after you've made any necessary changes.
     // You will need to use `\Drupal\core\Database\Database::getConnection()` if you do not yet have access to the container here.
     //Database::getConnection($options)->truncate('wisski_preview_images', $options)->execute();
