6ba6498ae0c2ee49db80315c0af3bd77f397f1db
rnsrk <rbrt.nsrk@posteo.de>
2021-12-06 09:23:15 +0100

    working on json upload

--- wisski_doi/src/Form/WisskiDOISchemeForm.php
+++ wisski_doi/src/Form/WisskiDOISchemeForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\wisski_doi\Form;
 
+use Drupal\Component\Utility\Environment;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -11,49 +12,49 @@ use Drupal\Core\Form\FormStateInterface;
  *
  * @internal
  */
-class WisskiDOISchemeForm extends FormBase {
+class WisskiDOISchemeForm extends FormBase
+{
 
   public function getFormId()
   {
     return 'wisski_doi_scheme_form';
   }
 
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
 
     $form = array();
 
+    //load exisiting data
     $settings = $this->configFactory()->getEditable('wisski_doi.wisski_doi_scheme_form');
 
+    // save existing data in form property
     $form['#wisski_doi_scheme_settings'] = $settings;
 
-    $form['schema'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Schema'),
-      '#default_value' => $settings->get('schema'),
-      '#description' => $this->t('The Schema.'),
-    ];
-
+    // create form elements
     $form['source'] = array(
       '#type' => 'fieldset',
       '#title' => t('Specify scheme file'),
       '#required' => TRUE,
       '#weight' => 2,
+      //upload possibility with json validator and maxupload size
       'upload' => array(
         '#type' => 'file',
         '#title' => t('File upload'),
-        // port to D8:
-        // we must explicitly set the extension validation to allow xml files
-        // to be uploaded.
-        // an empty array disables the extension restrictions:
-        // this is theoretically somewhat insecure but we get away with it ftm...
+        /*
         '#upload_validators' => array(
-          'file_validate_extensions' => array(),  // => array('xml')
+          'file_validate_extensions' => array('json'),
+          'file_validate_size' => [Environment::getUploadMaxSize()],
         ),
+        */
+        '#description' => $this->t('Only JSON files allowed, max filesize is %filesize.', ['%filesize' => format_size(Environment::getUploadMaxSize())]),
       ),
+      // simple text area
       'paste' => array(
         '#type' => 'textarea',
         '#title' => $this->t('Direct paste'),
-        '#rows' => 4,
+        '#rows' => 20,
+        '#default_value' => $settings->get('schema'),
       ),
     );
 
@@ -67,17 +68,31 @@ class WisskiDOISchemeForm extends FormBase {
 
   }
 
+
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+      $upload = $form_state->getValue('upload');
+
+      dpm(json_decode($upload));
+      $paste = $form_state->getValue('paste');
+      if (empty($upload) && empty($paste)) {
+        $form_state->setErrorByName('upload', $this->t('You have to provide a file or insert a direct paste.'));
+        $form_state->setErrorByName('paste', $this->t('You have to provide a file or insert a direct paste.'));
+        $continue = FALSE;
+      }
+    }
+
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
     $settings = $form['#wisski_doi_scheme_settings'];
     $new_vals = $form_state->getValues();
     dpm($new_vals);
-    if (!empty($new_vals['paste'])) {
-      $settings->set('schema', $new_vals['paste']);
-      $this->messenger()->addStatus($this->t('Saved Schema from direct paste.'));
-    } elseif (!empty($new_vals['upload'])) {
-      $settings->set('schema', $new_vals['upload']);
+    if (!empty($new_vals['upload'])) {
+      $settings->set('paste', $new_vals['upload']);
       $this->messenger()->addStatus($this->t('Saved Schema from upload.'));
+    } elseif
+    (!empty($new_vals['paste'])) {
+      $settings->set('paste', $new_vals['paste']);
+      $this->messenger()->addStatus($this->t('Saved Schema from direct paste.'));
     } else {
       $this->messenger()->addStatus($this->t('Please provide a schema!'));
     }
