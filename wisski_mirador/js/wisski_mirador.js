5e7ba4eb057dc36841be23bcb4d2ecf090cb0b11
root <m.fichtner@wiss-ki.eu>
2021-12-21 00:03:50 +0100

    Added annotation for IIIF images in Mirador 3

--- wisski_mirador/js/wisski_mirador.js
+++ wisski_mirador/js/wisski_mirador.js
@@ -11,14 +11,63 @@
 
           console.log('yay', drupalSettings.wisski.mirador.data);          
           console.log('yay', drupalSettings.wisski.mirador.windowObjects);
-        
-//          $(function() {
-//            jQuery = jQuery19;
-//            $ = jQuery19;          
+
+//import Mirador from 'mirador/dist/es/src/index';
+//import { miradorImageToolsPlugin } from 'mirador-image-tools';
+//import annotationPlugin from 'mirador-annotations';
+//import LocalStorageAdapter from 'mirador-annotations/lib/LocalStorageAdapter'
+
+          let plugins = [];    
+//          console.log(window.miradorPlugins);
+          if (window.miradorPlugins && window.miradorPlugins.length) {
+            console.log(window.miradorPlugins);
+//            window.miradorPlugins.        
+            for (let {plugin, name} of window.miradorPlugins) {	
+              //if (window.globalMiradorPlugins.includes(name)) {                
+                plugins = [...plugins, ...plugin];            
+              //}          
+            }
+          }
 //            alert(jQuery.fn.jquery);
+        const annotationEndpoint = '/wisski/mirador-annotations';
         const mirador = Mirador.viewer({
+/*
+          annotation: {
+            adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
+            //adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
+            // adapter: (canvasId) => new AnnototAdapter(canvasId, endpointUrl),
+            exportLocalStorageAnnotations: false, // display annotation JSON export button
+          },*/
+          annotation: {
+            adapter: (canvasId) => window.miradorAnnotationServerAdapter(canvasId, annotationEndpoint),
+          },
           id: "viewer",
           allowFullscreen: true,
+          "window": {
+            "allowClose": false,
+            "allowFullscreen": true,
+            "allowMaximize": false,
+            "allowTopMenuButton": true,
+            "allowWindowSideBar": true,
+            "sideBarPanel": "info",
+            "defaultSideBarPanel": "attribution",
+            "sideBarOpenByDefault": false,
+            "defaultView": "single",
+            "forceDrawAnnotations": true,
+            "hideWindowTitle": true,
+            "highlightAllAnnotations": false,
+            "showLocalePicker": true,
+            "sideBarOpen": true,
+            "switchCanvasOnSearch": true,
+            "panels": {
+              "info": true,
+              "attribution": true,
+              "canvas": true,
+              "annotations": true,
+              "search": true,
+              "layers": true
+            }
+          },
           windows: drupalSettings.wisski.mirador.data, //[
             //{ manifestId: "https://wisskid9.gnm.de/wisski/navigate/426/iiif_manifest" },
             //{ manifestId: "https://wisskid9.gnm.de/wisski/navigate/269/iiif_manifest" },
@@ -35,7 +84,7 @@
 //          ]
           // All of the settings (with descriptions (ﾉ^∇^)ﾉﾟ) located here:
           // https://github.com/ProjectMirador/mirador/blob/master/src/config/settings.js
-        });
+        }, plugins);
 
 /*
             Mirador({
