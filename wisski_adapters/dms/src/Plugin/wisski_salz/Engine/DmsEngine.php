9472e9386affa9791efaa67fad9547b0416196a3
root <m.fichtner@wiss-ki.eu>
2022-02-09 11:17:49 +0100

    Added more precision

--- wisski_adapters/dms/src/Plugin/wisski_salz/Engine/DmsEngine.php
+++ wisski_adapters/dms/src/Plugin/wisski_salz/Engine/DmsEngine.php
@@ -373,7 +373,8 @@ class DmsEngine extends NonWritableEngineBase implements PathbuilderEngineInterf
 #        dpm($data);
       foreach($keys as $step) {
         if(isset($a_ret[$step])) {
-          $a_ret[$step] = htmlspecialchars_decode($a_ret[$step]);
+          if(!is_object($a_ret[$step]))
+            $a_ret[$step] = htmlspecialchars_decode($a_ret[$step]);
           $data['Object'][$step] = array($a_ret[$step]);
         } else {
           $data['Object'][$step] = array();
