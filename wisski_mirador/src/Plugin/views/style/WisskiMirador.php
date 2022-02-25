1a2f4f47499836c79b8df5654072dfcea4e08647
Robert Nasarek <r.nasarek@gnm.de>
2022-02-25 21:58:27 +0100

    made switch base url autom to https

--- wisski_mirador/src/Plugin/views/style/WisskiMirador.php
+++ wisski_mirador/src/Plugin/views/style/WisskiMirador.php
@@ -7,7 +7,7 @@ use Drupal\views\Plugin\views\style\StylePluginBase;
 
 
 /**
- * Style plugin to render a mirador viewer as a 
+ * Style plugin to render a mirador viewer as a
  * views display style.
  *
  * @ingroup views_style_plugins
@@ -61,20 +61,20 @@ class WisskiMirador extends StylePluginBase {
               "layers": true
             }
       }');
-    
+
     return $options;
   }
-  
+
   public function buildOptionsForm(&$form, FormStateInterface $form_state) {
     parent::buildOptionsForm($form, $form_state);
-    
+
     $form['enable_annotations'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Enable Annotations'),
       '#description' => $this->t('This enables annotation storage with Mirador. Annotations are stored locally and you have to define a mapping in Drupal to an associated entity here.'),
       '#default_value' => $this->options['enable_annotations'],
     ];
-    
+
     $form['entity_type_for_annotation'] = [
       '#title' => $this->t('Entity type for annotations'),
       '#description' => $this->t('The machine name of the entity type the annotations are stored to.'),
@@ -82,7 +82,7 @@ class WisskiMirador extends StylePluginBase {
       '#size' => '50',
       '#default_value' => $this->options['entity_type_for_annotation'],
     ];
-    
+
     $form['bundle_for_annotation'] = [
       '#title' => $this->t('Bundle for annotations'),
       '#description' => $this->t('The machine id of the bundle the annotations are stored to. The bundle needs fields to store the annotations which are defined below.'),
@@ -105,7 +105,7 @@ class WisskiMirador extends StylePluginBase {
       '#type' => 'textfield',
       '#size' => '50',
       '#default_value' => $this->options['field_for_annotation_json'],
-    ];    
+    ];
 
     $form['field_for_annotation_entity'] = [
       '#title' => $this->t('Field for annotation entity'),
@@ -114,7 +114,7 @@ class WisskiMirador extends StylePluginBase {
       '#size' => '50',
       '#default_value' => $this->options['field_for_annotation_entity'],
     ];
-    
+
     $form['field_for_annotation_reference'] = [
       '#title' => $this->t('Field for annotation reference'),
       '#description' => $this->t('This field (machine name) stores the referred entity the annotation is created upon (e.g. the object that the above file depicts).'),
@@ -122,17 +122,17 @@ class WisskiMirador extends StylePluginBase {
       '#size' => '50',
       '#default_value' => $this->options['field_for_annotation_reference'],
     ];
-    
+
     $form['window_settings'] = [
       '#title' => $this->t('window'),
       '#description' => $this->t('Settings for Mirador "window" parameter'),
       '#type' => 'textarea',
       '#default_value' => $this->options['window_settings'],
     ];
-    
+
     return $form;
   }
-  
+
   public function usesFields() {
     return TRUE;
   }
@@ -151,10 +151,10 @@ class WisskiMirador extends StylePluginBase {
     if (!$fields) {
       $errors[] = $this->t('No fields are selected for display purpose. For Mirador you either need to display entity id (then the manifests get loaded automatically) or you need to display a field with a reference (http...) to an IIIF manifest.');
     }
-    
+
     if(empty($errors))
       parent::validate();
-    
+
     return $errors;
   }
 
@@ -164,20 +164,20 @@ class WisskiMirador extends StylePluginBase {
   public function render() {
 
     $view = $this->view;
-    
+
 #    dpm($view->field);
-    
+
     $results = $view->result;
-    
+
 #    dpm($results);
-    
+
     $ent_list = array();
     $direct_load_list = array();
-    
+
     $site_config = \Drupal::config('system.site');
-    
+
     $to_print = "";
-    
+
     if(!empty($site_config->get('name')))
       $to_print .= $site_config->get('name');
     if(!$site_config->get('slogan')) {
@@ -187,23 +187,27 @@ class WisskiMirador extends StylePluginBase {
         $to_print .= $site_config->get('slogan');
       }
     }
-        
-    global $base_url;
-    
+
+    if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+      $base_url = $_SERVER["HTTP_X_FORWARDED_PROTO"] . '://' . $_SERVER['HTTP_HOST'];
+    } else {
+      $base_url = 'http://' . $_SERVER['HTTP_HOST'];
+    }
+
     if(empty($to_print))
-      $to_print .= $base_url;   
-    
+      $to_print .= $base_url;
+
     $iter = 0;
     $result_count = count($results);
-    
+
     foreach($results as $result) {
-    
+
 #      dpm($result->eid);
 #      return;
 #      dpm(serialize($this->view));
 #      dpm($result->__get('entity:wisski_individual/eid'), "res?");
 
-      
+
       if(isset($result->eid))
         $entity_id = $result->eid;
       else
@@ -217,8 +221,8 @@ class WisskiMirador extends StylePluginBase {
           }
         }
       }
-        
-#      $entity_id = empty($result->eid) ? if(!empty(current($result->__get('entity:wisski_individual/eid'))) { current($result->__get('entity:wisski_individual/eid')) } : $result->eid; 
+
+#      $entity_id = empty($result->eid) ? if(!empty(current($result->__get('entity:wisski_individual/eid'))) { current($result->__get('entity:wisski_individual/eid')) } : $result->eid;
 
 #      dpm($result, "res?");
 #      $ent_list[] = array("manifestId" => $base_url . "/wisski/navigate/" . $entity_id . "/iiif_manifest", "manifestUri" => $base_url . "/wisski/navigate/" . $entity_id . "/iiif_manifest", "location" => $to_print);
@@ -237,20 +241,20 @@ class WisskiMirador extends StylePluginBase {
         $field_to_load_http_uri_from = current($field_to_load_http_uri_from);
 
 #        dpm($result->$field_to_load_http_uri_from);
-        
+
         $result_field = $result->$field_to_load_http_uri_from;
-        
+
 #        dpm($result_field);
         $result_field = current($result_field);
         if(!empty($result_field["value"]))
           $result_field = $result_field["value"];
         else
           $result_field = $result_field[0]["value"];
-        
+
         $ent_list[] = array("manifestId" => $result_field);
-        
+
 #        dpm($ent_list, "ente?");
-        
+
       }
 #      $direct_load_list[] = array( "loadedManifest" => $base_url . "/wisski/navigate/" . $result->eid . "/iiif_manifest", "availableViews" => array( 'ImageView'), "windowOptions" => array( "zoomLevel" => 1, "osdBounds" => array(
 #            "height" => 1500,
@@ -258,11 +262,11 @@ class WisskiMirador extends StylePluginBase {
 #            "x" => 1000,
 #            "y" => 2000,
 #        )), "slotAddress" => "row1.column" . ++$iter, "viewType" => "ImageView", "bottomPanel" => false, "sidePanel" => false, "annotationLayer" => false);
-    } 
-    
+    }
+
     if(isset($view->attachment_before)) {
       $attachments = $view->attachment_before;
-      
+
       foreach($attachments as $attachment) {
         $subview = $attachment['#view'];
 
@@ -274,6 +278,7 @@ class WisskiMirador extends StylePluginBase {
           $entity_id = empty($res->eid) ? current($res->__get('entity:wisski_individual/eid')) : $res->eid;
           $ent_list[] = array("manifestId" => $base_url . "/wisski/navigate/" . $entity_id . "/iiif_manifest");
 #          $ent_list[] = array("manifestId" => $base_url . "/wisski/navigate/" . $entity_id . "/iiif_manifest", "manifestUri" => $base_url . "/wisski/navigate/" . $entity_id . "/iiif_manifest", "location" => $to_print);
+          dpm($base_url);
           if ($subcount > 1) {
             $direct_load_list[] = array( "loadedManifest" => $base_url . "/wisski/navigate/" . $entity_id . "/iiif_manifest", "availableViews" => array( 'ImageView'), "slotAddress" => "row1.column" . ++$iter, "viewType" => "ImageView", "bottomPanel" => false, "sidePanel" => false, "annotationLayer" => false );
           }
@@ -281,7 +286,7 @@ class WisskiMirador extends StylePluginBase {
             $direct_load_list[] = array( "loadedManifest" => $base_url . "/wisski/navigate/" . $entity_id . "/iiif_manifest", "availableViews" => array( 'ImageView'), "viewType" => "ImageView", "bottomPanel" => false, "sidePanel" => false, "annotationLayer" => false );
           }
 #          $direct_load_list[] = array( "loadedManifest" => $base_url . "/wisski/navigate/" . $res->eid . "/iiif_manifest", "viewType" => "ImageView" );
-#          $direct_load_list[] = array( "loadedManifest" => $base_url . "/wisski/navigate/" . $res->eid . "/iiif_manifest", "availableViews" => array( 'ImageView'), "windowOptions" => array( "zoomLevel" => 1, "osdBounds" => array( 
+#          $direct_load_list[] = array( "loadedManifest" => $base_url . "/wisski/navigate/" . $res->eid . "/iiif_manifest", "availableViews" => array( 'ImageView'), "windowOptions" => array( "zoomLevel" => 1, "osdBounds" => array(
 #            "height" => 2000,
 #            "width" => 2000,
 #            "x" => 1000,
@@ -290,10 +295,10 @@ class WisskiMirador extends StylePluginBase {
         }
 
 //        dpm($subview->result, "resi!");
-        
+
       }
     }
-    
+
 #    dpm($ent_list, "ente gut...");
 
     $layout = count($ent_list);
@@ -305,22 +310,22 @@ class WisskiMirador extends StylePluginBase {
     } else {
       $layout_str = "1x1";
     }
-    
+
 #    foreach($ent_list as $ent
-    
-    
+
+
     $form = array();
-    
+
     $form['#attached']['drupalSettings']['wisski']['mirador']['data'] = $ent_list;
     $form['#attached']['drupalSettings']['wisski']['mirador']['options'] = $this->options;
-    
+
     $form['#attached']['drupalSettings']['wisski']['mirador']['window_settings'] = json_decode($this->options['window_settings'], TRUE);
     $form['#attached']['drupalSettings']['wisski']['mirador']['layout'] = $layout_str;
 
     if($layout < 9) {
       $form['#attached']['drupalSettings']['wisski']['mirador']['windowObjects'] = $direct_load_list;
     }
-        
+
     $form['#markup'] = '<div id="viewer"></div>';
     $form['#allowed_tags'] = array('div', 'select', 'option','a', 'script');
 #    #$form['#attached']['drupalSettings']['wisski_jit'] = $wisski_individual;
@@ -330,10 +335,10 @@ class WisskiMirador extends StylePluginBase {
     $session->set('mirador-options', $this->options);
 
     return $form;
-  
+
   }
-  
-  
-  
-  
-}     
+
+
+
+
+}
