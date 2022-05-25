6953e8ded9c424bbb3fc6707ad6e5148e5acdd75
root <m.fichtner@wiss-ki.eu>
2022-05-25 14:17:17 +0200

    revised changes - they were wrong...

--- wisski_odbc_import/src/Form/WisskiODBCImportForm.php
+++ wisski_odbc_import/src/Form/WisskiODBCImportForm.php
@@ -377,7 +377,7 @@ class WisskiODBCImportForm extends FormBase {
       // build the count query
       $tablename = isset($table->name) ? (string) $table->name : '';
       $append = isset($table->append) ? (string) $table->append : '';
-      $sql = "SELECT COUNT(*) FROM $tablename $append";
+      $sql = "SELECT COUNT(*) FROM `$tablename` $append";
     }
 
     // do the db query; distinguish if local connection or not
@@ -425,7 +425,7 @@ class WisskiODBCImportForm extends FormBase {
       $select = isset($table->select) ? (string) $table->select : '';
       if(empty($append))
         $append = "";
-      $sql = "SELECT $select FROM $tablename $append";
+      $sql = "SELECT $select FROM `$tablename` $append";
     }
     if ($limit) {
       $sql .= " LIMIT $limit";
