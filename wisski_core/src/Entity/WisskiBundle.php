2583c7a657a5517a51a139fea62483799da76b90
root <m.fichtner@wiss-ki.eu>
2023-06-28 09:14:31 +0200

    Added more bulletproofness to title generation

--- wisski_core/src/Entity/WisskiBundle.php
+++ wisski_core/src/Entity/WisskiBundle.php
@@ -150,6 +150,8 @@ class WisskiBundle extends ConfigEntityBundleBase implements WisskiBundleInterfa
 
       if(!empty($title))
         return $title;
+      else 
+        \Drupal::messenger()->addStatus('Could not create Title for Bundle ' . $this->id . '. Please resave the title pattern');
     }
 
     return unserialize($this->title_pattern);
