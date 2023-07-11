8f6dcd5a48b187a616b82f8b4b0ef2d54aaf6a73
Robert Nasarek <rbrt.nsrk@posteo.de>
2023-07-11 09:53:16 +0200

    add capsulation

--- wisski_core/js/wisski_autocomplete.js
+++ wisski_core/js/wisski_autocomplete.js
@@ -1,42 +1,45 @@
 /**
- * @file
- * Expands the behaviour of the default autocompletion.
- */
+* @file
+* Expands the behaviour of the default autocompletion.
+*/
 
 (function ($, Drupal) {
 
-  // On Drupal 10.x, we use a different approach to autocomplete.
-  // So disable this code if there isn't an autocomplete.
-  if (!Object.prototype.hasOwnProperty.call(Drupal, 'autocomplete')) {
-    return
-  }
+  Drupal.behaviors.wisskiAutocomplete = {
+    attach: function (context, settings) {
 
-  // Override the "select" option of the jQueryUI autocomplete
-  // to make sure we do not use quotes for inputs with comma.
-  Drupal.autocomplete.options.select = function (event, ui) {
+      // On Drupal 10.x, we use a different approach to autocomplete.
+      // So disable this code if there isn't an autocomplete.
+      if (!Object.prototype.hasOwnProperty.call(Drupal, 'autocomplete')) {
+        return
+      }
 
-    //var terms = Drupal.autocomplete.splitValues(event.target.value);
-    // start empty, otherwise cases with a , do not get created correctly...
-    var terms = [];
+      // Override the "select" option of the jQueryUI autocomplete
+      // to make sure we do not use quotes for inputs with comma.
+      Drupal.autocomplete.options.select = function (event, ui) {
 
-    // Remove the current input.
-    //terms.pop();
-    // Add the selected item.
-    terms.push(ui.item.value);
-    event.target.value = terms.join(', ');
-    // Return false to tell jQuery UI that we've filled in the value already.
-    return false;
-  };
+        //var terms = Drupal.autocomplete.splitValues(event.target.value);
+        // start empty, otherwise cases with a , do not get created correctly...
+        var terms = [];
 
- //Overrides the default extractLastTerm found in core/misc/autocomplete.js
-  Drupal.autocomplete.extractLastTerm = function extractLastTerm(terms) {
-    return terms;
-    //alert(terms);
-    //alert("yay?");
+        // Remove the current input.
+        //terms.pop();
+        // Add the selected item.
+        terms.push(ui.item.value);
+        event.target.value = terms.join(', ');
+        // Return false to tell jQuery UI that we've filled in the value already.
+        return false;
+      };
+
+      //Overrides the default extractLastTerm found in core/misc/autocomplete.js
+      Drupal.autocomplete.extractLastTerm = function extractLastTerm(terms) {
+        return terms;
+        //alert(terms);
+        //alert("yay?");
         //Implement your override behavior here
-  };
-
-
+      };
+    }
+  }
 })(jQuery, Drupal);
 
 
